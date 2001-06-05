f2c36457353fd37fb8cc91b71799ab75f336291f
Daniel Stenberg <daniel@haxx.se>
2001-06-05 08:05:04 +0000

    legal stuff files

---
+++ legal/header.c
@@ -0,0 +1,23 @@
+/*****************************************************************************
+ *                                  _   _ ____  _     
+ *  Project                     ___| | | |  _ \| |    
+ *                             / __| | | | |_) | |    
+ *                            | (__| |_| |  _ <| |___ 
+ *                             \___|\___/|_| \_\_____|
+ *
+ * Copyright (C) 2000, Daniel Stenberg, <daniel@haxx.se>, et al.
+ *
+ * In order to be useful for every potential user, curl and libcurl are
+ * dual-licensed under the MPL and the MIT/X-derivate licenses.
+ *
+ * You may opt to use, copy, modify, merge, publish, distribute and/or sell
+ * copies of the Software, and permit persons to whom the Software is
+ * furnished to do so, under the terms of the MPL or the MIT/X-derivate
+ * licenses. You may pick one of these licenses.
+ *
+ * As a courtesy to the open-source and free software community, we ask you to
+ * dual-license any modifications that you make as well, as specified in the
+ * LEGAL document.
+ *
+ * $Id$
+ *****************************************************************************/
