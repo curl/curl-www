09ff8555ca56f677ba60592c7dc331bf7ab94e05
Daniel Stenberg <daniel@haxx.se>
2001-11-06 07:25:31 +0000

    removed special package for cygwin sources, it builds out-of-the-box from the standard source archive!

--- latest.pm
+++ latest.pm
@@ -144,11 +144,6 @@ sub scanstatus {
             storemax($_, $1, $size, "cygwin-ssl",
                      "Windows archive for cygwin, bzip2, SSL-enabled");
         }
-        # curl-7.8.1-3-src-cygwin.tar.bz2
-        elsif($_ =~ /^curl-([0-9.]*)-(\d*)-src-cygwin.tar.bz2/) {
-            storemax($_, $1, $size, "cygwin-src",
-                     "Source archive for cygwin, bzip2, SSL-enabled");
-        }
     }
 }
 
