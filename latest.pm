58049793e17cd11657005c68bad4a1921a5977da
Daniel Stenberg <daniel@haxx.se>
2001-11-21 14:35:39 +0000

    modified for better re-use of the pattern matches

--- latest.pm
+++ latest.pm
@@ -43,41 +43,35 @@ sub storemax {
 
 }
 
-sub scanstatus {
+# return Version, Short-Type, Description
 
-    opendir(DIR, $dir) || die "can't opendir $dir: $!";
-    my @curls = grep { /^curl/ && -f "$dir/$_" } readdir(DIR);
-    closedir DIR;
+sub gettype {
+    my ($file)=@_;
 
-    for(@curls) {
-        my ($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,
-            $atime,$mtime,$ctime,$blksize,$blocks)
-            = stat("$dir/$_");
-        
-        # curl-7.5.1-win32-nossl.zip
-        if($_ =~ /^curl-([0-9.]*)-win32-nossl.zip/) {
-            storemax($_, $1, $size,
-                     "win32-nossl",
-                     "Windows archive, zip compressed.");
-        }
-        elsif($_ =~ /^curl-([0-9.]*)-win32-ssl.zip/) {
-            storemax($_, $1, $size, "win32-ssl",
-                     "Windows archive, zip compressed, SSL-enabled.");
-        }
-        elsif($_ =~ /^curl-([0-9.]*).tar.gz/) {
-            storemax($_, $1, $size, "tar.gz",
+    # curl-7.5.1-win32-nossl.zip
+    if($file =~ /^curl-([0-9.]*)-win32-nossl.zip/) {
+        return($1,
+                 "win32-nossl",
+                 "Windows archive, zip compressed.");
+    }
+    elsif($file =~ /^curl-([0-9.]*)-win32-ssl.zip/) {
+        return($1, "win32-ssl",
+                 "Windows archive, zip compressed, SSL-enabled.");
+    }
+    elsif($file =~ /^curl-([0-9.]*).tar.gz/) {
+            return($1, "tar.gz",
                      "Source tar archive, gzip compressed.");
         }
-        elsif($_ =~ /^curl-([0-9.]*).tar.bz2/) {
-            storemax($_, $1, $size, "tar.bz2",
+        elsif($file =~ /^curl-([0-9.]*).tar.bz2/) {
+            return($1, "tar.bz2",
                      "Source tar archive, bzip2 compressed.");
         }
-        elsif($_ =~ /^curl-([0-9.]*).zip/) {
-            storemax($_, $1, $size, "zip",
+        elsif($file =~ /^curl-([0-9.]*).zip/) {
+            return($1, "zip",
                      "Source archive, zip compressed.");
         }
         # curl-7.4.1-1.i386.rpm
-        elsif($_ =~ /^curl-([0-9.]*)-(\d*)(.*).i386.rpm/) {
+        elsif($file =~ /^curl-([0-9.]*)-(\d*)(.*).i386.rpm/) {
             my $pkg="i386.rpm";
             my $desc="Linux i386 RPM package. (redhat 6.2 style)";
             if($3 eq "rh71") {
@@ -88,20 +82,20 @@ sub scanstatus {
                 $pkg="rh72-".$pkg;
                 $desc="Linux i386 RPM package. (redhat 7.2 style)";
             }
-            storemax($_, $1, $size, $pkg, $desc);
+            return($1, $pkg, $desc);
         }
         # curl-7.4.1-1.ppc.rpm
-        elsif($_ =~ /^curl-([0-9.]*)-(\d*).ppc.rpm/) {
-            storemax($_, $1, $size, "ppc.rpm",
+        elsif($file =~ /^curl-([0-9.]*)-(\d*).ppc.rpm/) {
+            return($1, "ppc.rpm",
                      "Linux PPC RPM package.");
         }
         # curl-7.4.1-1.src.rpm
-        elsif($_ =~ /^curl-([0-9.]*)-(\d*).src.rpm/) {
-            storemax($_, $1, $size, "src.rpm",
+        elsif($file =~ /^curl-([0-9.]*)-(\d*).src.rpm/) {
+            return($1, "src.rpm",
                      "Source Linux RPM archive.");
         }
         # curl-ssl-7.4.1-1.i386.rpm
-        elsif($_ =~ /^curl-ssl-([0-9.]*)-(\d*)(.*).i386.rpm/) {
+        elsif($file =~ /^curl-ssl-([0-9.]*)-(\d*)(.*).i386.rpm/) {
             my $pkg="ssl-i386.rpm";
             my $desc="Linux i386 RPM package, SSL-enabled. (redhat 6.2 style)";
             if($3 eq "rh71") {
@@ -112,37 +106,58 @@ sub scanstatus {
                 $pkg="rh72-".$pkg;
                 $desc="Linux i386 RPM package, SSL-enabled. (redhat 7.2 style)";
             }
-            storemax($_, $1, $size, $pkg, $desc);
+            return($1, $pkg, $desc);
         }
         # curl-ssl-7.4.1-1.ppc.rpm
-        elsif($_ =~ /^curl-ssl-([0-9.]*)-(\d*).ppc.rpm/) {
-            storemax($_, $1, $size, "ssl-ppc.rpm",
+        elsif($file =~ /^curl-ssl-([0-9.]*)-(\d*).ppc.rpm/) {
+            return($1, "ssl-ppc.rpm",
                      "Source Linux RPM archive, SSL-enabled.");
         }
         # curl-ssl-7.4.1-1.src.rpm
-        elsif($_ =~ /^curl-ssl-([0-9.]*)-(\d*).src.rpm/) {
-            storemax($_, $1, $size, "ssl-src.rpm",
+        elsif($file =~ /^curl-ssl-([0-9.]*)-(\d*).src.rpm/) {
+            return($1, "ssl-src.rpm",
                      "Source Linux RPM archive, SSL-enabled.");
         }
         # curl-ssl-7.8.1-sparc-8-pkg.tar.gz
-        elsif($_ =~ /^curl-ssl-([0-9.]*)-sparc-8.pkg.tar.gz/) {
-            storemax($_, $1, $size, "solaris8-sparc-ssl",
+        elsif($file =~ /^curl-ssl-([0-9.]*)-sparc-8.pkg.tar.gz/) {
+            return($1, "solaris8-sparc-ssl",
                      "Solaris 8 SPARC archive, SSL-enabled.");
         }
         # curl-ssl-7.9-sparc-2.6-pkg.tar.gz
-        elsif($_ =~ /^curl-ssl-([0-9.]*)-sparc-2.6.pkg.tar.gz/) {
-            storemax($_, $1, $size, "solaris26-sparc-ssl",
+        elsif($file =~ /^curl-ssl-([0-9.]*)-sparc-2.6.pkg.tar.gz/) {
+            return($1, "solaris26-sparc-ssl",
                      "Solaris 2.6 SPARC archive, SSL-enabled.");
         }
         # curl-7.8.1-vms.zip
-        elsif($_ =~ /^curl-([0-9.]*)-vms\.zip/) {
-            storemax($_, $1, $size, "vms-zip",
+        elsif($file =~ /^curl-([0-9.]*)-vms\.zip/) {
+            return($1, "vms-zip",
                      "OpenVMS archive, zip compressed.");
         }
         # curl-7.8.1-2-cygwin.tar.bz2
-        elsif($_ =~ /^curl-([0-9.]*)-(\d*)-cygwin.tar.bz2/) {
-            storemax($_, $1, $size, "cygwin-ssl",
+        elsif($file =~ /^curl-([0-9.]*)-(\d*)-cygwin.tar.bz2/) {
+            return($1, "cygwin-ssl",
                      "Windows archive for cygwin, bzip2, SSL-enabled");
+        }
+
+}
+
+sub scanstatus {
+
+    opendir(DIR, $dir) || die "can't opendir $dir: $!";
+    my @curls = grep { /^curl/ && -f "$dir/$_" } readdir(DIR);
+    closedir DIR;
+
+    for(@curls) {
+        my $file = $_;
+
+        my ($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,
+            $atime,$mtime,$ctime,$blksize,$blocks)
+            = stat("$dir/$_");
+
+        my ($version, $short, $full)=gettype($_);
+
+        if($version) {
+            storemax($file, $version, $size, $short, $full);
         }
     }
 }
