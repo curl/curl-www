952d9ffc9f2e60e2e447633e0dd734f596ae0cda
Daniel Stenberg <daniel@haxx.se>
2001-11-21 22:49:49 +0000

    cygwin-src

--- latest.pm
+++ latest.pm
@@ -138,6 +138,11 @@ sub gettype {
             return($1, "cygwin-ssl",
                      "Windows archive for cygwin, bzip2, SSL-enabled");
         }
+        # curl-7.8.1-2-src-cygwin.tar.bz2
+        elsif($file =~ /^curl-([0-9.]*)-(\d*)-src-cygwin.tar.bz2/) {
+            return($1, "cygwin-src",
+                     "Source archive for cygwin, bzip2, SSL-enabled");
+        }
 
 }
 
