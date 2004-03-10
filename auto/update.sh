56c04ed711d326de0ddeb0377e6f5d82c196f750
Daniel Stenberg <daniel@haxx.se>
2004-03-10 16:05:18 +0000

    fix

--- auto/update.sh
+++ auto/update.sh
@@ -5,7 +5,7 @@ PATH="/userdir/dast/linux/bin:/usr/local/bin:/bin:/usr/bin:/usr/sbin:/sbin"
 export PATH
 
 # first remove oldies
-find inbox -mtime +20 -exec rm {} \;
+find inbox -mtime +18 -exec rm {} \;
 
 ./summarize.pl 
 
@@ -17,4 +17,5 @@ if [ -s dump ]; then
   ./cvslast5.pl < dump > ./cvs.t 2>/dev/null
 fi
 
-make -k 2>/dev/null
+make -k >/dev/null 2>&1
+
