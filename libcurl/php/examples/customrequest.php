d55d942af034f56223c00df48f74315dc982021a
Daniel Stenberg <daniel@haxx.se>
2004-04-22 13:04:01 +0000

    narrower

--- libcurl/php/examples/customrequest.php
+++ libcurl/php/examples/customrequest.php
@@ -7,7 +7,8 @@ $ch = curl_init();
 
 curl_setopt($ch, CURLOPT_URL,"http://www.mysite.com/postit.cgi");
 curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, "postvar1=value1&postvar2=value2&postvar3=value3");
+curl_setopt($ch, CURLOPT_POSTFIELDS,
+            "postvar1=value1&postvar2=value2&postvar3=value3");
 
 // issue a FOOBAR request instead of POST!
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "FOOBAR");
