d03318c6b860d80cea8aa04898633d1bfdca7f31
Daniel Stenberg <daniel@haxx.se>
2004-04-22 12:54:29 +0000

    customrequest example

---
+++ libcurl/php/examples/customrequest.php
@@ -0,0 +1,17 @@
+<?php
+//
+// A very simple POST example with a custom request keyword 'FOOBAR'
+//
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL,"http://www.mysite.com/postit.cgi");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, "postvar1=value1&postvar2=value2&postvar3=value3");
+
+// issue a FOOBAR request instead of POST!
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "FOOBAR");
+
+curl_exec ($ch);
+curl_close ($ch); 
+?>
