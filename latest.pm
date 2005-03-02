9b889ce8376bee495c8cf9b8d75e7a1e3e732b95
Daniel Stenberg <daniel@haxx.se>
2005-03-02 23:32:24 +0000

    fixed msvc regex without ssl, removed dots

--- latest.pm
+++ latest.pm
@@ -52,31 +52,35 @@ sub gettype {
     if($file =~ /^curl-([0-9.]*)(-([0-9]*)|)-win32-nossl.zip$/) {
         return($1,
                "win32-nossl",
-               "Windows, zip.");
+               "Windows, zip");
     }
     elsif($file =~ /^curl-([0-9.]*)(-([0-9]*)|)-win32-ssl.zip$/) {
         return($1, "win32-ssl",
-                 "Windows, zip, SSL-enabled.");
+                 "Windows, zip, SSL-enabled");
     }
     elsif($file =~ /^curl-([0-9.]*)(-([0-9]*)|)-win32-ssl-devel-mingw32.zip$/) {
         return($1, "win32-ssl-devel-mingw",
-                 "Windows mingw devel, zip, SSL-enabled.");
+                 "Windows mingw devel, zip, SSL-enabled");
     }
     elsif($file =~ /^curl-([0-9.]*)(-([0-9]*)|)-win32-ssl-devel.zip$/) {
         return($1, "win32-ssl-devel-msvc",
-                 "Windows MSVC devel, zip, SSL-enabled.");
+                 "Windows MSVC devel, zip, SSL-enabled");
+    }
+    elsif($file =~ /^curl-([0-9.]*)(-([0-9]*)|)-win32-devel.zip$/) {
+        return($1, "win32-devel-msvc",
+                 "Windows MSVC devel, zip");
     }
     elsif($file =~ /^curl-([0-9.]*).tar.gz$/) {
         return($1, "tar.gz",
-               "Generic source tar, gzip.");
+               "Generic source tar, gzip");
     }
     elsif($file =~ /^curl-([0-9.]*).tar.bz2$/) {
         return($1, "tar.bz2",
-               "Generic source tar, bzip2.");
+               "Generic source tar, bzip2");
     }
     elsif($file =~ /^curl-([0-9.]*).zip$/) {
         return($1, "zip",
-               "Generic source, zip.");
+               "Generic source, zip");
     }
     # curl-7.4.1-1.i386.rpm
     elsif($file =~ /^curl-([0-9.]*)-(\d*)(.*).i386.rpm$/) {
@@ -87,17 +91,17 @@ sub gettype {
     # curl-7.4.1-1.ppc.rpm
     elsif($file =~ /^curl-([0-9.]*)-(\d*).ppc.rpm$/) {
         return($1, "ppc.rpm",
-               "Linux PPC RPM.");
+               "Linux PPC RPM");
     }
     # curl-devel-7.4.1-1.ppc.rpm
     elsif($file =~ /^curl-devel-([0-9.]*)-(\d*).ppc.rpm$/) {
         return($1, "devel-ppc.rpm",
-               "Linux PPC devel RPM.");
+               "Linux PPC devel RPM");
     }
     # curl-7.4.1-1.src.rpm
     elsif($file =~ /^curl-([0-9.]*)-(\d*).src.rpm$/) {
         return($1, "src.rpm",
-               "Linux source RPM.");
+               "Linux source RPM");
     }
     # curl-ssl-7.4.1-1.i386.rpm
     #elsif($file =~ /^curl-ssl-([0-9.]*)-(\d*)(.*).i386.rpm$/) {
@@ -121,56 +125,56 @@ sub gettype {
     # curl-ssl-7.4.1-1.ppc.rpm
     elsif($file =~ /^curl-ssl-([0-9.]*)-(\d*).ppc.rpm$/) {
         return($1, "ssl-ppc.rpm",
-               "Linux PPC RPM, SSL-enabled.");
+               "Linux PPC RPM, SSL-enabled");
     }
     # curl-ssl-devel-7.4.1-1.ppc.rpm
     elsif($file =~ /^curl-ssl-devel-([0-9.]*)-(\d*).ppc.rpm$/) {
         return($1, "ssl-devel-ppc.rpm",
-               "Linux PPC devel RPM, SSL-enabled.");
+               "Linux PPC devel RPM, SSL-enabled");
     }
     # curl-ssl-7.4.1-1.src.rpm
     #elsif($file =~ /^curl-ssl-([0-9.]*)-(\d*).src.rpm$/) {
     #    return($1, "ssl-src.rpm",
-    #           "Linux source RPM, SSL-enabled.");
+    #           "Linux source RPM, SSL-enabled");
     #}
     # curl-ssl-7.8.1-sparc-8-pkg.tar.gz
     elsif($file =~ /^curl-ssl-([0-9.]*)-sparc-8.pkg.tar.gz/) {
         return($1, "solaris8-sparc-ssl",
-               "Solaris 8 SPARC, gzip, SSL-enabled.");
+               "Solaris 8 SPARC, gzip, SSL-enabled");
     }
     # curl-ssl-7.8.1-sparc-8-pkg.tar.gz
     elsif($file =~ /^curl-ssl-([0-9.]*)-sparc-8.pkg.tar.bz2$/) {
         return($1, "solaris8-sparc-ssl-bz2",
-               "Solaris 8 SPARC, bzip2, SSL-enabled.");
+               "Solaris 8 SPARC, bzip2, SSL-enabled");
     }
     # curl-ssl-7.9-sparc-2.6-pkg.tar.gz
     elsif($file =~ /^curl-ssl-([0-9.]*)-sparc-2.6.pkg.tar.gz$/) {
         return($1, "solaris26-sparc-ssl",
-               "Solaris 2.6 SPARC, SSL-enabled.");
+               "Solaris 2.6 SPARC, SSL-enabled");
     }
     # curl-7.8.1-vms.zip (old style)
 #    elsif($file =~ /^curl-([0-9.]*)-vms\.zip$/) {
 #        return($1, "vms-zip",
-#               "OpenVMS, zip.");
+#               "OpenVMS, zip");
 #    }
     # curl-7.8.1-vms-vax.zip
     elsif($file =~ /^curl-([0-9.]*)-vms-vax\.zip$/) {
         return($1, "vms-vax-zip",
-               "OpenVMS VAX, zip.");
+               "OpenVMS VAX, zip");
     }
     # curl-7.8.1-vms-ia64.zip
     elsif($file =~ /^curl-([0-9.]*)-vms-ia64\.zip$/) {
         return($1, "vms-ia64-zip",
-               "OpenVMS ia64, zip.");
+               "OpenVMS ia64, zip");
     }
     # curl-7.8.1-vms-axp.zip
     elsif($file =~ /^curl-([0-9.]*)-vms-axp\.zip$/) {
         return($1, "vms-axp-zip",
-               "OpenVMS Alpha, zip.");
+               "OpenVMS Alpha, zip");
     }
     elsif($file =~ /^curl-([0-9.]*)-arm\.tar\.gz$/) {
         return($1, "linux-arm-nossl",
-               "Linux ARM, SSL disabled, tar+gz.");
+               "Linux ARM, SSL disabled, tar+gz");
     }
 
     # Kevin's new formats starting with curl 7.10:
