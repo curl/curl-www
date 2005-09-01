088253e75ee97b2a60b8e8e058d53f0894d42c55
Daniel Stenberg <daniel@haxx.se>
2005-09-01 15:13:41 +0000

    fix the dilemma doc

---
+++ legal/dilemma.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+while(<STDIN>) {
+    if($_ =~ s/((http|ftp):\/\/([A-Za-z0-9.\/_%\#-]*[0-9A-Za-z\/]))/<a href=\"$2:\/\/$3\">$1<\/a>/g) {
+    }
+    else {
+        # convert <> to html-codes
+        $_ =~ s/\</&lt;/g;
+        $_ =~ s/\>/&gt;/g;
+
+        # empty lines become &nbsp;
+        $_ =~ s/^[ \t]*$/\&nbsp;/g;
+
+        # trailing backslashes become \&nbsp;
+        $_ =~ s/\\$/\\&nbsp;/;
+
+        # convert the begining of a "C comment" to a html code to prevent the
+        # cpp to barf
+        $_ =~ s/\/\*/&\#47;*/g;
+    }
+    print $_;
+}
