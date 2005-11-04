ee28374f92b2957f367784f511d771284a4f3e8b
Daniel Stenberg <daniel@haxx.se>
2005-11-04 08:47:21 +0000

    add script here and make all of the site use this

---
+++ docs/txt2plain.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+while(<STDIN>) {
+    # convert <> to html-codes
+    $_ =~ s/\</&lt;/g;
+    $_ =~ s/\>/&gt;/g;
+
+    # empty lines become &nbsp;
+    $_ =~ s/^[ \t]*$/\&nbsp;/g;
+
+    # trailing backslashes become \&nbsp;
+    $_ =~ s/\\$/\\&nbsp;/;
+
+    # convert the begining of a "C comment" to a html code to prevent the
+    # cpp to barf
+    $_ =~ s/\/\*/&\#47;*/g;
+
+    # convert URLs to <a href>
+    $_ =~ s/((http|ftp):\/\/([a-zA-Z0-9.=\/_%?-]*[0-9A-Za-z\/]))/<a href=\"$2:\/\/$3\">$1<\/a>/g;
+
+    print $_;
+}
