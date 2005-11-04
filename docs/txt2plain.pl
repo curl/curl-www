72e1a1cbb02d9892c1490b9def876937cb88bcfe
Daniel Stenberg <daniel@haxx.se>
2005-11-04 09:00:46 +0000

    convert # on first column

--- docs/txt2plain.pl
+++ docs/txt2plain.pl
@@ -15,6 +15,9 @@ while(<STDIN>) {
     # cpp to barf
     $_ =~ s/\/\*/&\#47;*/g;
 
+    # convert ^#
+    $_ =~ s:^ *\#:&\#35:;
+
     # convert URLs to <a href>
     $_ =~ s/((http|ftp):\/\/([a-zA-Z0-9.=\/_%?-]*[0-9A-Za-z\/]))/<a href=\"$2:\/\/$3\">$1<\/a>/g;
 
