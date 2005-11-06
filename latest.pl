82a0ccd2411b23b799c18812f5cf810441987d07
Daniel Stenberg <daniel@haxx.se>
2005-11-06 22:59:15 +0000

    added curl.triplemind.com

--- latest.pl
+++ latest.pl
@@ -44,6 +44,7 @@ my %mirrors=('ftp://ftp.sunet.se/pub/www/utilities/curl/' => 'Sweden (Uppsala)',
              'http://curl.mirror-server.net/download/' => 'Germany (Nuremberg)',
              'http://curl.hoxt.com/download/' => 'US (Florida)',
              'http://curl.nedmirror.nl/download/' => 'Netherlands (Amsterdam)',
+             'http://curl.triplemind.com/' => 'Germany (Mannheim)'
              );
 
 sub present {
