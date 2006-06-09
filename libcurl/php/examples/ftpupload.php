aac985e4ebdcb7a456f06fb3d927ac667dd3dde9
Daniel Stenberg <daniel@haxx.se>
2006-06-09 07:44:05 +0000

    rectified the ftpupload script somewhat

--- libcurl/php/examples/ftpupload.php
+++ libcurl/php/examples/ftpupload.php
@@ -4,18 +4,19 @@
 //
 
 $ch = curl_init();
+$localfile = "me-and-my-dog.jpg"
+
+$fp = fopen ($localfile, "r");
 
 // we upload a JPEG image
 curl_setopt($ch, CURLOPT_URL,
             "ftp://mynamw:mypassword@ftp.mysite.com/path/to/destination.jpg");
 curl_setopt($ch, CURLOPT_UPLOAD, 1);
-curl_setopt($ch, CURLOPT_INFILE, "me-and-my-dog.jpg");
+curl_setopt($ch, CURLOPT_INFILE, $fp);
 
 // set size of the image, which isn't _mandatory_ but helps libcurl to do
-// extra error checking on the upload. If you can tell me how to get filesize
-// nicely with a PHP function then I'd like to add it here to make the example
-// better!
-curl_setopt($ch, CURLOPT_INFILESIZE, 3271);
+// extra error checking on the upload.
+curl_setopt($ch, CURLOPT_INFILESIZE, filesize($localfile));
 
 $error = curl_exec ($ch);
 
