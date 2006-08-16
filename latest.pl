cc09a2b6d9b2766bb4ce91191ebb04aaf09b6ad9
Daniel Stenberg <daniel@haxx.se>
2006-08-16 17:54:32 +0000

    new Russian mirror

--- latest.pl
+++ latest.pl
@@ -59,6 +59,7 @@ my %mirrors=('ftp://ftp.sunet.se/pub/www/utilities/curl/' => 'Sweden (Uppsala)',
              'http://curl.de-mirror.de/download/' => 'Germany (Aachen)',
              'http://curl.webdesign-zdg.de/download/' => 'Germany (Frankfurt)',
              'http://curl.oslevel.de/download/' => 'Germany (Karlsruhe)',
+             'http://curl.gfiles.org/download/' => 'Russia (Vladivostok)',
              );
 
 sub present {
