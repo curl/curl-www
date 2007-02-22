5a33b9432e8ac322c3e11b07c9672da2224bffef
Daniel Stenberg <daniel@haxx.se>
2007-02-22 14:21:14 +0000

    detect and mark builds with libssh2

--- auto/explainopts.t
+++ auto/explainopts.t
@@ -1,6 +1,6 @@
 <p> Options = (<b>K</b>)rb4, IPv(<b>6</b>), (<b>D</b>)ebug, Open(<b>S</b>)SL,
 Gnu(<b>T</b>)LS, (<b>A</b>)synch, (<b>Z</b>)lib, (<b>G</b>)SS,
-lib(<b>I</b>)dn, SS(<b>P</b>)I
+lib(<b>I</b>)dn, SS(<b>P</b>)I, libssh(<b>2</b>)
 
 <p> Text <span class='buildserverprob'>looking like this</span> is for servers
 that couldn't be started for the test round.
--- auto/summarize.pl
+++ auto/summarize.pl
@@ -313,8 +313,9 @@ sub endofsingle {
     $https=($openssl)?"S":($gnutls?"T":"-");
     $asynch=$ares?"A":"-";
     $sspi=$sspi?"P":"-";
+    my $ssh=$libssh2?"2":"-";
 
-    my $o = "$krb4$ipv6$memory$https$asynch$zlib$gss$idn$sspi";
+    my $o = "$krb4$ipv6$memory$https$asynch$zlib$gss$idn$sspi$ssh";
 
     if(!$desc) {
         $desc = $os;
@@ -353,6 +354,7 @@ sub endofsingle {
     $debug=0;
     $openssl=0;
     $gnutls=0;
+    $libssh2=0;
     $ssl=0;
     $cvsfail=0;
     $ares=0;
@@ -505,6 +507,9 @@ sub singlefile {
             }
             elsif($line =~ /^\#define USE_GNUTLS 1/) {
                 $gnutls = 1;
+            }
+            elsif($line =~ /^\#define USE_LIBSSH2 1/) {
+                $libssh2 = 1;
             }
             elsif($line =~ /^\#define ENABLE_IPV6 1/) {
                 $ipv6enabled = 1;
