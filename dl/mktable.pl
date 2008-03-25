6e3d39bd43649e7857e621cff6ff8e44c781f8e7
Dan Fandrich <dan@coneharvesters.com>
2008-03-25 18:14:16 +0000

    Added MIME types to some archive links and links that could be confused with archive links.

--- dl/mktable.pl
+++ dl/mktable.pl
@@ -213,7 +213,7 @@ for $per (@sall) {
     my $mirror;
     my $metalink;
     if($$per{'re'} ne "-") {
-        $mirror="<a href=\"http://curl.haxx.se/latest.cgi?curl=$$per{'re'}\" title=\"download mirrors\">";
+        $mirror="<a href=\"http://curl.haxx.se/latest.cgi?curl=$$per{'re'}\" type=\"text/html\" title=\"download mirrors\">";
         $metalink="<a href=\"http://curl.haxx.se/metalink.cgi?curl=$$per{'re'}\" type=\"application/metalink+xml\">" .
                   "<img src=\"/pix/metalink.png\" border=\"0\" alt=\"metalink\" title=\"metalink\"></a>";
     }
