8fda79b210199f7a60cd80626fdc9d3914117b21
Daniel Stenberg <daniel@haxx.se>
2009-03-02 13:42:20 +0000

    cvsadd vuln.pm and depend on it properly in the makefile

---
+++ docs/vuln.pm
@@ -0,0 +1,10 @@
+# datestamp, first vulnerable version, last vulnerable version
+@vuln = ("20070710|7.14.0|7.16.3|libcurl GnuTLS insufficient cert verification",
+         "BID17154|7.15.0|7.15.2|libcurl TFTP Packet Buffer Overflow",
+         "BID15756|7.11.2|7.15.0|libcurl URL Buffer Overflow",
+         "BID15102|7.10.6|7.14.1|libcurl NTLM Buffer Overflow",
+         "BID12616|7.3   |7.13.0|Kerberos Authentication Buffer Overflow",
+         "BID12615|7.10.6|7.13.0|NTLM Authentication Buffer Overflow",
+         "BID8432 |7.1   |7.10.6|Proxy Authentication Header Information Leakage",
+         "BID1804 |6.0   |7.4   |FTP Server Response Buffer Overflow",
+    );
