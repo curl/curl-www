c3a7df1d4525393cc0ac5d5e20db658bc3d70da8
Daniel Stenberg <daniel@haxx.se>
2009-03-02 13:41:34 +0000

    cut off the "since" from the vulnerabilities table, point to the release log, use the shared vuln.pm file

--- docs/vulntable.pl
+++ docs/vulntable.pl
@@ -3,15 +3,7 @@
 # un-preprocessed _changes-file as input
 
 # datestamp, first vulnerable version, last vulnerable version
-my @vuln = ("20070710|7.14.0|7.16.3",
-            "BID17154|7.15.0|7.15.2",
-            "BID15756|7.11.2|7.15.0",
-            "BID15102|7.10.6|7.14.1",
-            "BID12616|7.3   |7.13.0",
-            "BID12615|7.10.6|7.13.0",
-            "BID8432 |7.1   |7.10.6",
-            "BID1804 |6.0   |7.4   ",
-            );
+require "vuln.pm";            
             
 sub vernum {
     my ($ver)=@_;
@@ -24,12 +16,12 @@ sub head {
     print "<tr class=\"tabletop\"><th>index</th><th>Version</th>";
     my $v=1;
     for(@vuln) {
-        my ($id, $start, $stop)=split('\|');
+        my ($id, $start, $stop, $desc)=split('\|');
         $id =~ s/ //;
-        print "<th title=\"$id\"><a href=\"/docs/security.html\#$id\">#$v</a></th>";
+        print "<th title=\"$id - $desc\"><a href=\"/docs/security.html\#$id\">#$v</a></th>";
         $v++;
     }
-    print "<th>Release Date</th><th>Since Most Recent</th></tr>\n";
+    print "<th>Release Date</th></tr>\n";
 }
 
 head();
@@ -61,42 +53,13 @@ while(<STDIN>) {
 
         $anchor =~ s/\./_/g;
 
-        my $datesecs=`date -d "$date" +%s`;
-        my $daysbetween;
-
-        if($prevsecs) {
-            # number of seconds between two releases!
-            my $reltime = $prevsecs - $datesecs;
-
-            # convert to days
-            $daysbetween = int($reltime/(3600*24));
-
-            if($daysbetween < 100) {
-                $age = "$daysbetween days";
-            }
-            elsif($daysbetween < 400) {
-                $age = sprintf("%d months", int($daysbetween/30));
-            }
-            else {
-                my $mon = int(($daysbetween%365)/30);
-                $age = sprintf("%d years, %d months", 
-                               int($daysbetween/365),
-                               $mon);
-            }
-        }
-        else {
-            # store the first date
-            $prevsecs = $datesecs;
-            $age="most recent";
-        }
-
         printf("<tr class=\"%s\"><td>%d</td><td><a href=\"/changes.html#$anchor\">$str</a></td>",
                $l&1?"even":"odd",
                $index++);
         for my $i (0 .. scalar(@vuln)-1 ) {
             printf("<td%s</td>", $v[$i]?" style=\"background-color: #f00000;\">yes":">&nbsp;");
         }
-        print "<td>$date</td><td>$age</td></tr>";
+        print "<td>$date</td></tr>";
 
         if(!(++$l % 25)) {
             head();
