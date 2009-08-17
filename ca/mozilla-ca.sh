ced05508e363d5db699e3a74efac29d63b575e55
Daniel Stenberg <daniel@haxx.se>
2009-08-17 12:56:14 +0000

    run the new script instead

--- ca/mozilla-ca.sh
+++ ca/mozilla-ca.sh
@@ -1,20 +1,6 @@
 #!/bin/sh
-
-PATH="/home/dast/linux/bin:/home/dast/bin:/usr/local/bin:/bin:/usr/bin:/usr/sbin:/sbin"
-
-export PATH
-
-cd $HOME/curl_html/ca
-
-curl -s "http://lxr.mozilla.org/seamonkey/source/security/nss/lib/ckfw/builtins/certdata.txt?raw=1" -o certdata.txt
-
-if $? -gt 0; then
-  exit curl failed
-fi
-
 FILE=cacert.pem-foo
-
-./parse-certs.sh certdata.txt $FILE > /dev/null
+perl ./mk-ca-bundle.pl -u $FILE
 
 if [ -s $FILE ]; then
   mv $FILE cacert.pem
