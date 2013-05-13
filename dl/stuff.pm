4bcd08adff3a0f193a9e143ede1045a69ff581b7
Dan Fandrich <dan@coneharvesters.com>
2013-05-13 18:38:25 +0200

    Add a meta header to set UTF-8 encoding on remcheck logs
    The web server doesn't send an encoding and Chrome, at least,
    guesses wrong.

--- dl/stuff.pm
+++ dl/stuff.pm
@@ -26,6 +26,7 @@ sub getheader {
     push @head, <<MOO
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html><head><title>$title</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
 <link rel="stylesheet" type="text/css" href="/curl.css">
 </head>
 <body>
