e54fd167ea39736986eba2952e53e123139953c2
Daniel Stenberg <daniel@haxx.se>
2014-06-18 11:30:53 +0200

    opts: args added missing files from previos commit

--- libcurl/c/mkopts.pl
+++ libcurl/c/mkopts.pl
@@ -8,6 +8,7 @@ closedir DIR;
 
 my $actions = "Makefile.opts";
 my $targets = "Makefile.opttargets";
+my $index = "alloptions.t";
 
 unlink $actions;
 
@@ -39,15 +40,34 @@ moo
     close(M);
 }
 
+my %desc;
+sub shortdesc {
+    my ($name) = @_;
+    open (M, "<$dir/$name.3");
+    my $state;
+    while(<M>) {
+        if($_ =~ /^\.SH NAME/) {
+            $state++;
+        }
+        elsif($_ =~ /^$name[\\ ]*-(.*)/ && ($state == 1)) {
+            $desc{$name}=$1;
+            last;
+        }
+    }
+    close(M);
+}
+
 for (@opts) {
     my $f = $_;
     $f =~ s/\.3//; # cut off the extention to get the symbol name
     push @all, $f;
     single($f);
     makeit($f);
+    shortdesc($f);
 }
 
 open(TG, ">$targets");
+open(IDX, ">$index");
 print TG "OPTPAGES = ";
 my $c = 0;
 for(sort @all) {
@@ -56,5 +76,8 @@ for(sort @all) {
     }
     print TG "  $_.html";
     $c++;
+
+    printf IDX "<br> <a href=\"$_.html\">$_</a> - %s\n", $desc{$_};
 }
 close(TG);
+close(IDX);
