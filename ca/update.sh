dbbeeb127de9a19ea207f659749930bbfa2fd76d
Daniel Stenberg <daniel@haxx.se>
2016-02-19 10:55:42 +0100

    ca/update: save a datestamped copy when the PEM is updated

--- ca/update.sh
+++ ca/update.sh
@@ -3,6 +3,11 @@
 # get the cert and create ca-bundle.crt
 perl ../cvssource/lib/mk-ca-bundle.pl
 
-bzip2 -c cacert.pem > cacert.pem.bz2
-gzip -c cacert.pem > cacert.pem.gz
-
+md5sum -c check
+if test $? -gt "0"; then
+    # PEM was updated, save this by date
+    cp cacert.pem cacert-`date +%Y-%m-%d`.pem
+    md5sum cacert.pem > check
+    bzip2 -c cacert.pem > cacert.pem.bz2
+    gzip -c cacert.pem > cacert.pem.gz
+fi
