62859381a91c7d5b736adad40afa4e3eb9d60c7b
Daniel Stenberg <daniel@haxx.se>
2017-04-04 15:10:36 +0200

    CVE-2017-7407

--- docs/vuln.pm
+++ docs/vuln.pm
@@ -1,5 +1,6 @@
 # page, first vulnerable version, last vulnerable version, name, CVE, date (YYYYMMDD)
 @vuln = (
+    "adv_20170403.html|6.5|7.53.1|--write-out out of buffer read|CVE-2017-7407|20170403",
     "adv_20170222.html|7.52.0|7.52.1|SSL_VERIFYSTATUS ignored|CVE-2017-2629|20170222",
     "adv_20161223.html|7.52.0|7.52.0|uninitialized random|CVE-2016-9594|20161223",
     "adv_20161221A.html|7.1|7.51.0|printf floating point buffer overflow|CVE-2016-9586|20161221",
