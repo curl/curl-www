ef8c6dd98751f2ffe3de0f770ac60b30ea7be2fc
Daniel Stenberg <daniel@haxx.se>
2017-04-10 11:08:30 +0200

    adv_20170419: TLS session resumption client cert bypass (again)
    libcurl would attempt to resume a TLS session even if the client
    certificate had changed. That is unacceptable since a server by
    specification is allowed to skip the client certificate check on resume,
    and may instead use the old identity which was established by the
    previous certificate (or no certificate).
    
    CVE-2017-7468
    
    Bug: https://curl.haxx.se/docs/adv_20170419.html

--- docs/vuln.pm
+++ docs/vuln.pm
@@ -1,5 +1,6 @@
 # page, first vulnerable version, last vulnerable version, name, CVE, date (YYYYMMDD)
 @vuln = (
+    "adv_20170419.html|7.52.0|7.53.1|TLS session resumption client cert bypass (again)|CVE-2017-XXXX|20170419",
     "adv_20170403.html|6.5|7.53.1|--write-out out of buffer read|CVE-2017-7407|20170403",
     "adv_20170222.html|7.52.0|7.52.1|SSL_VERIFYSTATUS ignored|CVE-2017-2629|20170222",
     "adv_20161223.html|7.52.0|7.52.0|uninitialized random|CVE-2016-9594|20161223",
