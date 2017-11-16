d871569ffd1b8ead6dff127d120f013f98003edb
Daniel Stenberg <daniel@haxx.se>
2017-11-16 17:43:32 +0100

    ca/update: generate sha256sum

--- ca/update.sh
+++ ca/update.sh
@@ -3,13 +3,13 @@
 # get the cert and create ca-bundle.crt
 perl ../cvssource/lib/mk-ca-bundle.pl
 
-md5sum -c check
+sha256sum -c cacert.pem.sha256
 if test $? -gt "0"; then
     # PEM was updated, save this by date
     cp cacert.pem cacert-`date +%Y-%m-%d`.pem
-    md5sum cacert.pem > check
-    bzip2 -c cacert.pem > cacert.pem.bz2
+    sha256sum cacert.pem > cacert.pem.sha256
     gzip -c cacert.pem > cacert.pem.gz
+    xz -c cacert.pem > cacert.pem.xz
     perl ./listpem.pl > pemlist.gen
     make
 fi
