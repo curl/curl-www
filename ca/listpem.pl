327d05de28fb9a628c7357ec36ee355aa5b12e39
Daniel Stenberg <daniel@haxx.se>
2018-08-20 14:30:45 +0200

    ca/listpem: show a table, only include 10 entries
    For older ca bundles, shown on the caextract page.

--- ca/listpem.pl
+++ ca/listpem.pl
@@ -4,8 +4,31 @@ opendir(my $dh, ".") || die "can't opendir: $!";
 my @pems = grep { /^cacert-.*pem/ } readdir($dh);
 closedir $dh;
 
+sub countcerts {
+    my ($f)=@_;
+    open(F, "<$f");
+    my $cert;
+    while(<F>) {
+        if($_ =~ /^-----BEGIN/) {
+            $cert++;
+        }
+    }
+    return $cert;
+}
+
+print "<table><tr><th>Date</th><th>Certificates</th><tr>\n";
+my $l = 0;
 foreach my $p (reverse sort @pems) {
     if($p =~ /cacert-(.*).pem/) {
-        print "<li> <a href=\"/ca/$p\">$1</a>\n";
+        my $n = countcerts($p);
+        my $date = $1;
+        printf "<tr %s><td><a href=\"/ca/$p\">%s</a></td> <td align=center>%d</td></tr>\n",
+            $l&1?"class=\"odd\"":"",
+            $date, $n;
+        if(++$l >= 10) {
+            # only show 10
+            last;
+        }
     }
 }
+print "</table>\n";
