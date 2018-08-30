f677ec52b4fb53879a12995198d16cf12a0e8701
Daniel Gustafsson <dgustafsson@pivotal.io>
2018-08-31 00:39:30 +0200

    Close tableheader element with proper tag
    The Vulns column was errenously closed with </a> and not </th>.

--- docs/relinfo.pl
+++ docs/relinfo.pl
@@ -19,7 +19,7 @@ sub head {
     }
     print "<tr class=\"tabletop\"><th rowspan=2>#</th><th rowspan=2>Version</th>";
     printf("<th rowspan=2>Date</th><th rowspan=2>Since %s</th><th colspan=3>Delta</th><th colspan=4>Accumulated</th></tr>\n", $releases[0]);
-    print "<tr class=\"tabletop\"><th>Days</th><th>Bugfixes</th><th>Changes</th><th>Days</th><th>Bugfixes</th><th>Changes</th><th>Vulns</a></tr>\n";
+    print "<tr class=\"tabletop\"><th>Days</th><th>Bugfixes</th><th>Changes</th><th>Days</th><th>Bugfixes</th><th>Changes</th><th>Vulns</th></tr>\n";
 }
 
 my $l;
