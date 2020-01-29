71992b3d9e661bb3043343837a09dcb76b7212b4
Daniel Stenberg <daniel@haxx.se>
2020-01-29 09:08:24 +0100

    docs/Makefile: generate http-cookies with github-markup

--- setup.mk
+++ setup.mk
@@ -2,4 +2,5 @@ NOW=$(shell date +'-D__TODAY__=%B %d, %Y')
 ACTION=fcpp $(FCPP_OPTS) -I$(ROOT) -WWW -Uunix -P -H -C -V -LL "$(NOW)" $< $@;
 TXT2PLAIN= $(ROOT)/docs/txt2plain.pl
 MARKDOWN=markdown
+GHMARKDOWN=github-markup
 GITHUB=github-markup
