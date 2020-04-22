6bdba0b5d05bde4039fd167923383d81aec7ef40
Daniel Stenberg <daniel@haxx.se>
2020-04-22 09:56:44 +0200

    vuln2csv.pl: output the dates as YYYY-MM-DD

--- docs/vuln2csv.pl
+++ docs/vuln2csv.pl
@@ -4,5 +4,7 @@ require "./vuln.pm";
 
 for(@vuln) {
     my @a=split('\|');
+    $a[5] =~ s/(\d\d\d\d)(\d\d)(\d\d)/$1-$2-$3/;
+    $a[6] =~ s/(\d\d\d\d)(\d\d)(\d\d)/$1-$2-$3/;
     print join(";", @a),"\n";
 }
