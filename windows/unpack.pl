0eb83d25ae9f127044567954a7a475af763fab50
Daniel Stenberg <daniel@haxx.se>
2020-08-04 17:03:46 +0200

    windows/unpack: script that unpacks new uploads

---
+++ windows/unpack.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+
+my $uploadpath="/home/curl-for-win";
+my $pattern="^all-mingw-([0-9._]*).zip.txt";
+my $extract="dl";
+
+opendir(DIR, $uploadpath) || exit;
+my @ul = grep { /$pattern/ } readdir(DIR);
+closedir DIR;
+
+my @sul = reverse sort @ul;
+
+sub filetime {
+    my ($filename)=@_;
+
+    my ($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,
+        $atime,$mtime,$ctime,$blksize,$blocks)
+        = stat($filename);
+
+    return $ctime;
+}
+
+my $n;
+if($n = $sul[0]) {
+    if($n =~ /$pattern/) {
+        my $stamp = $1;
+        $n =~ s/\.txt//;
+        my $f = "$uploadpath/$n";
+        my $when = filetime($f);
+        printf "Uploaded: %s\n", $f;
+        printf "Stamp: %s\n", $stamp;
+        system "(mkdir $extract-$stamp && cd $extract-$stamp && unzip -q $f)";
+        system "echo $stamp > latest.txt";
+    }
+}
