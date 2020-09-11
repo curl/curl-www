104a2451737098976c307324713ea05dd11f2f6f
Daniel Stenberg <daniel@haxx.se>
2020-09-11 23:25:59 +0200

    vuln2csv: make no bounty get reported as 0 in the output
    Previously the script would leave out that column, which was unexpected.
    
    Fixes #76
    Reported-by: Peter Körner

--- docs/vuln2csv.pl
+++ docs/vuln2csv.pl
@@ -6,5 +6,6 @@ for(@vuln) {
     my @a=split('\|');
     $a[5] =~ s/(\d\d\d\d)(\d\d)(\d\d)/$1-$2-$3/;
     $a[6] =~ s/(\d\d\d\d)(\d\d)(\d\d)/$1-$2-$3/;
+    $a[8] += 0; # make sure it exists
     print join(";", @a),"\n";
 }
