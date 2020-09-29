8cbec7ed18f151738dded981af47a071cc607c85
Daniel Stenberg <daniel@haxx.se>
2020-09-29 23:23:02 +0200

    mkdash: insert generation time stamp last

--- mkdash.pl
+++ mkdash.pl
@@ -40,6 +40,13 @@ while(<S>) {
 }
 close(S);
 
+sub now {
+    my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) =
+        gmtime(time);
+    return sprintf "%04d-%02d-%02d %02d:%02d:%02d UTC",
+        $year + 1900, $mon + 1, $mday, $hour, $min, $sec;
+}
+
 for my $s (keys %svg) {
     my $alt = $s;
     $alt =~ s/-/ /g;
@@ -63,3 +70,5 @@ print <<BOTTOM
 #endif
 BOTTOM
     ;
+
+print "<br> Updated ".now()."\n";
