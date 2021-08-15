07ce11860db7ef8e58cfe7485fddea031b2fc7fe
Viktor Szakats <vszakats@users.noreply.github.com>
2021-08-15 04:47:47 +0200

    pix: upgrade icons8.png to SVG (#140)
    - the text part is a reproduction (same font, weight)
      to match the existing PNG logo
    - replace white background with transparent
    - trim empty borders

--- mk-sponsors.pl
+++ mk-sponsors.pl
@@ -16,7 +16,7 @@ my %silver = (
     "https://www.crosswordsolver.com" => 'CrosswordSolver.svg',
     "https://www.minitool.com" => 'minitool.png',
     "https://www.maid2clean.co.uk/domestic-cleaning/" => 'maid2clean.png',
-    "https://icons8.com" => 'icons8.png',
+    "https://icons8.com" => 'icons8.svg',
     );
 my %modurl = (
     );
