2ce90a1e08bd3e94e7d606e4e0f10149bed2eec4
Viktor Szakats <commit@vsz.me>
2022-08-13 20:20:47 +0000

    windows: deploy new "latest" download urls
    - Fix CDN/caching issues experienced with the previous "lastest" URL
      solution.
    - Add support for permalinking package signatures.
    - Stop refreshing symlinks for the previous-style URLs.
      Those will be deleted at the next curl release latest.
    
    New URL format:
    'https://curl.se/windows/latest.cgi?p=win64-mingw.zip'
    'https://curl.se/windows/latest.cgi?p=win64-mingw.zip.asc'
    'https://curl.se/windows/latest.cgi?p=win64-mingw.tar.xz'
    'https://curl.se/windows/latest.cgi?p=win64-mingw.tar.xz.asc'
    'https://curl.se/windows/latest.cgi?p=win64a-mingw.zip'
    'https://curl.se/windows/latest.cgi?p=win64a-mingw.zip.asc'
    'https://curl.se/windows/latest.cgi?p=win64a-mingw.tar.xz'
    'https://curl.se/windows/latest.cgi?p=win64a-mingw.tar.xz.asc'
    'https://curl.se/windows/latest.cgi?p=win32-mingw.zip'
    'https://curl.se/windows/latest.cgi?p=win32-mingw.zip.asc'
    'https://curl.se/windows/latest.cgi?p=win32-mingw.tar.xz'
    'https://curl.se/windows/latest.cgi?p=win32-mingw.tar.xz.asc'
    
    Reported-by: privetryan on github
    Co-authored-by: Daniel Stenberg
    
    Fixes https://github.com/curl/curl-www/issues/193

--- windows/unpack.pl
+++ windows/unpack.pl
@@ -39,13 +39,6 @@ if($n = $sul[0]) {
             printf "Stamp: %s\n", $stamp;
             system "(mkdir -p $extract-$stamp && cd $extract-$stamp && unzip -oq $f)";
             system "echo $stamp > $latest";
-            # update symlinks for "the latest"
-            system "ln -sf dl-$stamp/curl-$stamp-win64a-mingw.zip curl-win64a-latest.zip";
-            system "ln -sf dl-$stamp/curl-$stamp-win64-mingw.zip curl-win64-latest.zip";
-            system "ln -sf dl-$stamp/curl-$stamp-win32-mingw.zip curl-win32-latest.zip";
-            system "ln -sf dl-$stamp/curl-$stamp-win64a-mingw.tar.xz curl-win64a-latest.tar.xz";
-            system "ln -sf dl-$stamp/curl-$stamp-win64-mingw.tar.xz curl-win64-latest.tar.xz";
-            system "ln -sf dl-$stamp/curl-$stamp-win32-mingw.tar.xz curl-win32-latest.tar.xz";
         }
     }
 }
