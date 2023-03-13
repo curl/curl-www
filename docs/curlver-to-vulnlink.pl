745dc6708f196ed4a7dc5568f9e03d902dcf2b23
Daniel Stenberg <daniel@haxx.se>
2023-03-13 11:00:01 +0100

    curlver-to-vulnlink.pl: fix v8 links and 7.[single num] versions

--- docs/curlver-to-vulnlink.pl
+++ docs/curlver-to-vulnlink.pl
@@ -1,14 +1,12 @@
 #!/usr/bin/perl
 
 while(<STDIN>) {
-    if($_ =~ s/(7\.(\d+)\.(\d+))/<a href="vuln-$1.html">$1<\/a>/g) {
+    if($_ =~ s/([78]\.(\d+)\.(\d+))/<a href="vuln-$1.html">$1<\/a>/g) {
         # to avoid that 7.X matches a substring of a longer version like
         # 7.1 in 7.17.1
         ;
     }
-    else {
-        $_ =~ s/(7\.[1-9])[^0-9\.]/<a href="vuln-$1.html">$1<\/a>/g;
-    }
+    $_ =~ s/(7\.[1-9])([^0-9\.])/<a href="vuln-$1.html">$1<\/a>$2/g;
     $_ =~ s/(CURLOPT_([A-Z0-9_]+))/<a href="https:\/\/curl.se\/libcurl\/c\/$1.html">$1<\/a>/g;
     print $_;
 }
