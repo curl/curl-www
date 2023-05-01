6b4319c293404a45060e028e7da60f7461811f9a
Daniel Stenberg <daniel@haxx.se>
2023-05-01 12:03:42 +0200

    add: docs/vuln.json
    Generate and provide CVE information in JSON format
    
    Inspired by https://ossf.github.io/osv-schema/
    
    Closes #237

---
+++ docs/vuln2json.pl
@@ -0,0 +1,157 @@
+#!/usr/bin/perl
+
+require "./vuln.pm";
+
+sub jsonquote {
+    my ($in) = @_;
+
+    # escape double quotes and backslashes
+    $in =~ s/([\"\\])/\\$1/g;
+
+    # trim the trail
+    $in =~ s/\n+\z//g;
+
+    # trim the start
+    $in =~ s/^[ \n\t]+//;
+
+    # newlines
+    $in =~ s/\n/\\n/g;
+
+    return $in;
+}
+
+sub modified {
+    my ($cve)=@_;
+    my $date;
+    open(G, "git log --date=iso-strict -1 $cve.md|");
+    while(<G>) {
+        if(/^Date: +(.*)/) {
+            $date = $1;
+        }
+    }
+    close(G);
+    return $date;
+}
+
+sub scancve {
+    my ($cve)=@_;
+    my ($severity, $desc, $repby, $patchby, $fixed);
+    my $inc = 0;
+    open(F, "<$cve.md");
+    while(<F>) {
+        $_ =~ s/\r//;
+        if(/^- Not affected versions.* >= (.*)/) {
+            $fixed = $1;
+        }
+        elsif(/^- Patched-by: (.*)/i) {
+            $patchby = $1;
+        }
+        elsif(/^- Reported-by: (.*)/i) {
+            $repby = $1;
+        }
+        elsif(/^Severity: (.*)/i) {
+            $severity = ucfirst($1);
+        }
+        elsif(/^VULNERABILITY/) {
+            $inc = 1;
+        }
+        elsif(/^------/ && ($inc == 1)) {
+            $inc = 2; 
+        }
+        elsif($inc == 2) {
+            if(/^[A-Z ]+\n\z/) {
+                $inc = 0;
+            }
+            else {
+                $desc .= $_;
+            }
+        }
+    }
+    close(F);
+    if(!$fixed) {
+        die "could not find fixed in $cve";
+    }
+    return ($desc, $severity, $repby, $patchby, $fixed);
+}
+
+
+my $i=0;
+print "[\n";
+for(@vuln) {
+    my ($file, $first, $last, $name, $cve, $announce, $report,
+        $cwe, $award, $area, $cissue)=split('\|');
+    $announce =~ s/(\d\d\d\d)(\d\d)(\d\d)/$1-$2-$3/;
+    $report =~ s/(\d\d\d\d)(\d\d)(\d\d)/$1-$2-$3/;
+    $award += 0; # make sure it exists
+    my $modified = modified($cve);
+
+    my ($desc, $severity, $repby, $patchby, $fixed)=scancve($cve);
+
+    print ",\n" if($i);
+    print "  {\n".
+        "    \"id\": \"$cve\",\n".
+        "    \"summary\": \"$name\",\n".
+        "    \"URL\": \"https://curl.se/docs/$cve.html\",\n".
+        "    \"modified\": \"${modified}Z\",\n".
+        "    \"CWE\": \"$cwe\",\n".
+        "    \"published\": \"$announce\",\n".
+        "    \"affected\": [\n".
+        "      {\n".
+        "        \"package\": {\n".
+        "          \"name\": \"curl\",\n".
+        "          \"purl\": \"pkg:generic/curl\"\n".
+        "        },\n".
+        "        \"ranges\": [\n".
+        "          {\n".
+        "             \"type\": \"SEMVER\",\n".
+        "             \"events\": [\n".
+        "               {\"introduced\": \"$first\"},\n".
+        "               {\"last\": \"$last\"},\n".
+        "               {\"fixed\": \"$fixed\"}\n".
+        "             ]\n".
+        "          }\n".
+        "        ]\n".
+        "      }\n".
+        "    ],\n";
+    if($severity) {
+        my $cvss = "1.0"; # default low
+        $cvss = "4.0" if($severity eq "Medium");
+        $cvss = "7.0" if($severity eq "High");
+        $cvss = "9.0" if($severity eq "Critical");
+        print "    \"severity\": [\n".
+            "      {\n".
+            "        \"type\": \"basic\",\n".
+            "        \"score\": \"$severity\"\n".
+            "      },\n".
+            "      {\n".
+            "        \"type\": \"CVSS_V3\",\n".
+            "        \"score\": \"$cvss\"\n".
+            "      }\n".
+            "    ],\n";
+    }
+    print "    \"credits\": [\n";
+    if($repby) {
+        my $c = 0;
+        for my $r (split(/, /, $repby)) {
+            print ",\n" if($c);
+            print         "      {\n".
+                "        \"name\": \"$r\",\n".
+                "        \"type\": \"FINDER\"\n".
+                "      }";
+            $c++;
+        }
+        print "," if($patchby);
+        print "\n";
+    }
+    if($patchby) {
+        print         "      {\n".
+            "        \"name\": \"$patchby\",\n".
+            "        \"type\": \"REMEDIATION_DEVELOPER\"\n".
+            "      }\n";
+    }
+    print "    ],\n";
+    printf "    \"details\": \"%s\"\n".
+        "  }", jsonquote($desc);
+    $i++;
+}
+print "\n]\n";
