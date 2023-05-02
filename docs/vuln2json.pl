c57bfd5a2f976e5de093f682ff3864788e4fa3aa
Daniel Stenberg <daniel@haxx.se>
2023-05-02 09:45:09 +0200

    also generate an individual JSON for each flaw: "$CVE.json"

--- docs/vuln2json.pl
+++ docs/vuln2json.pl
@@ -33,6 +33,13 @@ sub modified {
     return $date;
 }
 
+sub dumpobj {
+    my ($cve, @json)=@_;
+    open(O, ">$cve.json");
+    print O @json;
+    close(O);
+}
+
 sub scancve {
     my ($cve)=@_;
     my ($severity, $desc, $repby, $patchby, $fixed);
@@ -74,9 +81,9 @@ sub scancve {
     return ($desc, $severity, $repby, $patchby, $fixed);
 }
 
-
+my @all;
 my $i=0;
-print "[\n";
+push @all, "[\n";
 for(@vuln) {
     my ($file, $first, $last, $name, $cve, $announce, $report,
         $cwe, $award, $area, $cissue)=split('\|');
@@ -84,74 +91,82 @@ for(@vuln) {
     $report =~ s/(\d\d\d\d)(\d\d)(\d\d)/$1-$2-$3/;
     $award += 0; # make sure it exists
     my $modified = modified($cve);
+    my @single;
 
     my ($desc, $severity, $repby, $patchby, $fixed)=scancve($cve);
 
-    print ",\n" if($i);
-    print "  {\n".
-        "    \"id\": \"$cve\",\n".
-        "    \"summary\": \"$name\",\n".
-        "    \"URL\": \"https://curl.se/docs/$cve.html\",\n".
-        "    \"modified\": \"${modified}Z\",\n".
-        "    \"CWE\": \"$cwe\",\n".
-        "    \"published\": \"$announce\",\n".
-        "    \"affected\": [\n".
-        "      {\n".
-        "        \"package\": {\n".
-        "          \"name\": \"curl\",\n".
-        "          \"purl\": \"pkg:generic/curl\"\n".
-        "        },\n".
-        "        \"ranges\": [\n".
-        "          {\n".
-        "             \"type\": \"SEMVER\",\n".
-        "             \"events\": [\n".
-        "               {\"introduced\": \"$first\"},\n".
-        "               {\"last\": \"$last\"},\n".
-        "               {\"fixed\": \"$fixed\"}\n".
-        "             ]\n".
-        "          }\n".
-        "        ]\n".
-        "      }\n".
-        "    ],\n";
+    push @all, ",\n" if($i);
+    push @single,
+        "{\n".
+        "  \"id\": \"$cve\",\n".
+        "  \"summary\": \"$name\",\n".
+        "  \"URL\": \"https://curl.se/docs/$cve.html\",\n".
+        "  \"modified\": \"${modified}Z\",\n".
+        "  \"CWE\": \"$cwe\",\n".
+        "  \"published\": \"$announce\",\n".
+        "  \"affected\": [\n".
+        "    {\n".
+        "      \"package\": {\n".
+        "        \"name\": \"curl\",\n".
+        "        \"purl\": \"pkg:generic/curl\"\n".
+        "      },\n".
+        "      \"ranges\": [\n".
+        "        {\n".
+        "           \"type\": \"SEMVER\",\n".
+        "           \"events\": [\n".
+        "             {\"introduced\": \"$first\"},\n".
+        "             {\"last\": \"$last\"},\n".
+        "             {\"fixed\": \"$fixed\"}\n".
+        "           ]\n".
+        "        }\n".
+        "      ]\n".
+        "    }\n".
+        "  ],\n";
     if($severity) {
         my $cvss = "1.0"; # default low
         $cvss = "4.0" if($severity eq "Medium");
         $cvss = "7.0" if($severity eq "High");
         $cvss = "9.0" if($severity eq "Critical");
-        print "    \"severity\": [\n".
-            "      {\n".
-            "        \"type\": \"basic\",\n".
-            "        \"score\": \"$severity\"\n".
-            "      },\n".
-            "      {\n".
-            "        \"type\": \"CVSS_V3\",\n".
-            "        \"score\": \"$cvss\"\n".
-            "      }\n".
-            "    ],\n";
+        push @single,
+            "  \"severity\": [\n".
+            "    {\n".
+            "      \"type\": \"basic\",\n".
+            "      \"score\": \"$severity\"\n".
+            "    },\n".
+            "    {\n".
+            "      \"type\": \"CVSS_V3\",\n".
+            "      \"score\": \"$cvss\"\n".
+            "    }\n".
+            "  ],\n";
     }
-    print "    \"credits\": [\n";
+    push @single, "  \"credits\": [\n";
     if($repby) {
         my $c = 0;
         for my $r (split(/, /, $repby)) {
-            print ",\n" if($c);
-            print         "      {\n".
-                "        \"name\": \"$r\",\n".
-                "        \"type\": \"FINDER\"\n".
-                "      }";
+            push @single, ",\n" if($c);
+            push @single,
+                "    {\n".
+                "      \"name\": \"$r\",\n".
+                "      \"type\": \"FINDER\"\n".
+                "    }";
             $c++;
         }
-        print "," if($patchby);
-        print "\n";
+        push @single, "," if($patchby);
+        push @single, "\n";
     }
     if($patchby) {
-        print         "      {\n".
-            "        \"name\": \"$patchby\",\n".
-            "        \"type\": \"REMEDIATION_DEVELOPER\"\n".
-            "      }\n";
+        push @single,
+            "    {\n".
+            "      \"name\": \"$patchby\",\n".
+            "      \"type\": \"REMEDIATION_DEVELOPER\"\n".
+            "    }\n";
     }
-    print "    ],\n";
-    printf "    \"details\": \"%s\"\n".
-        "  }", jsonquote($desc);
+    push @single, "  ],\n";
+    push @single, sprintf "  \"details\": \"%s\"\n".
+        "}", jsonquote($desc);
     $i++;
+    dumpobj($cve, @single);
+    push @all, @single;
 }
-print "\n]\n";
+push @all, "\n]\n";
+print @all;
