433e6266c0d918bd824bbae1de2e859ec44c3837
Daniel Stenberg <daniel@haxx.se>
2023-05-09 18:24:27 +0200

    Revert "setup: generate markdown HTML without wrapping"
    This reverts commit 1f755424ab5b58a9a9d885b6e78e69384b132fc8.

--- setup.mk
+++ setup.mk
@@ -1,4 +1,4 @@
 ACTION=fcpp $(FCPP_OPTS) -I$(ROOT) -WWW -Uunix -P -H -C -V -LL $< $@
 TXT2PLAIN=$(ROOT)/docs/txt2plain.pl
-MARKDOWN=pandoc -f gfm --wrap=none
+MARKDOWN=pandoc -f gfm
 GITHUB=pandoc -f gfm
