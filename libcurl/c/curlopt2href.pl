b9ab9b204ef42129eb4e03b70f8114a64a36743c
Daniel Stenberg <daniel@haxx.se>
2023-06-09 11:14:01 +0200

    curlopt2href.pl: link more symbols

--- libcurl/c/curlopt2href.pl
+++ libcurl/c/curlopt2href.pl
@@ -9,6 +9,7 @@ while(<STDIN>) {
     # skip if preceded by letters making it likely to be a link
     $_ =~ s/([^\">\/])(CURL(OPT|INFO|MOPT)_[A-Z_0-9]+)/$1<a href="$2.html">$2<\/a>/g;
     # also linkify libcurl function calls
-    $_ =~ s/([^\">\/]|^)(curl_(easy|curl|multi)_[a-z_]*)(\()/$1<a href="$2.html">$2<\/a>$4/g;
+    $_ =~ s/([^\">\/]|^)(curl_(url|ws|pushheader|global|version|slist|share|mime|easy|curl|multi)_[a-z_]*)(\()/$1<a href="$2.html">$2<\/a>$4/g;
+    $_ =~ s/([^\">\/]|^)(curl_(url|getdate|formfree|formadd|formget|free|escape|version))(\()/$1<a href="$2.html">$2<\/a>$4/g;
     print $_;
 }
