91d5f499ec82c2728c57e38b32dea82e97247314
Frank Gevaerts <frank.gevaerts@fks.be>
2023-06-23 12:47:32 +0200

    Fix cacert-${d}.pem.sha256 files to actually refer to the correct file.
    Currently they refer to the plain cacert.pem files, which makes
    "simple" checking fail.
    (reported by ursa on irc, with suggestions from Viktor Szakats)

--- ca/update.sh
+++ ca/update.sh
@@ -7,9 +7,9 @@ sha256sum -c cacert.pem.sha256
 if test $? -gt "0"; then
   # PEM was updated, save this by date
   d="$(date +%Y-%m-%d)"
-  cp cacert.pem "cacert-${d}.pem"
   sha256sum cacert.pem > cacert.pem.sha256
-  cp cacert.pem.sha256 "cacert-${d}.pem.sha256"
+  cp cacert.pem "cacert-${d}.pem"
+  sed "s/cacert.pem/cacert-${d}.pem/" < cacert.pem.sha256 > "cacert-${d}.pem.sha256"
   gzip -c cacert.pem > cacert.pem.gz
   xz -c cacert.pem > cacert.pem.xz
   perl ./listpem.pl > pemlist.gen
