08455667c170350ac87d67cbbcb6b4dcbf407daa
Daniel Stenberg <daniel@haxx.se>
2023-08-21 11:12:53 +0200

    docs: retract CVE-2023-32001
    This is no longer considered a curl security flaw. The CVE was
    officially rejected 2023-08-27 via HackerOne, our CNA.
    
    Ref: https://github.com/curl/curl/issues/11530
    
    Closes #284

--- docs/novuln.pm
+++ docs/novuln.pm
@@ -16,4 +16,5 @@
 @novuln = (
     "CVE-2019-15601.html|6.0|7.67.0|SMB access smuggling via FILE URL on Windows|CVE-2019-15601|20200108|20191031|CWE-20: Improper Input Validation|400",
     "CVE-2020-19909.html|-|-|Bogus report filed by anonymous|CVE-2020-19909|20230822|20230825|-|-|-|-",
+    "CVE-2023-32001.html|7.84.0|8.1.2|fopen race condition|CVE-2023-32001|20230719|20230627|CWE-367: Time-of-check Time-of-use (TOCTOU) Race Condition|2400|storage|-|both|medium",
     );
