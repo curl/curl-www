d32528d0457f7f977a599e74ffc5343c09a2ca08
Daniel Stenberg <daniel@haxx.se>
2023-09-07 16:48:44 +0200

    mk-sponsors.pl: minor polish

--- mk-sponsors.pl
+++ mk-sponsors.pl
@@ -13,7 +13,7 @@ my %silver = (
     "https://www.partitionwizard.com" => 'partitionwizard-2.svg',
     "https://www.crosswordsolver.com" => 'CrosswordSolver.svg',
     "https://www.minitool.com" => 'minitool-2.svg',
-    "https://icons8.com" => 'icons8.svg',
+    "https://icons8.com/" => 'icons8.svg',
     "https://serpapi.com" => 'serpapi.svg',
     'https://mowcapital.com' => 'mowcapital.svg',
     "https://proxy-hub.com/" => 'proxyhub.svg',
@@ -21,7 +21,7 @@ my %silver = (
     'https://www.sure.bet/casinos-not-on-gamstop/' => 'surebet.png',
     'https://iboysoft.com' => 'iBoysoft.svg',
     'https://fitmymoney.com/' => 'Fit-My-Money-vertical.svg',
-    'https://evpsys.com' => 'evp.png',
+    'https://evpsys.com' => 'evp.svg',
     'https://mobilen.nu/' => 'mobilen.png',
     # https://opencollective.com/ivan13 has a 'null' website in profile
     "null" => 'fineproxy.jpg',
@@ -58,7 +58,7 @@ for my $u (reverse @urls) {
 SPONSOR
                 ;
             if(! -f "pix/silver/$img") {
-                print STDERR "Missing image: $img\n";
+                print STDERR "Missing image file: pix/silver/$img\n";
                 exit 1;
             }
             $images++;
