b149108120efc1cbb5ac653c7014c732a1af4b63
Daniel Stenberg <daniel@haxx.se>
2023-12-01 14:37:10 +0100

    syminver: link (many) names to their man pages

--- libcurl/c/syminver.pl
+++ libcurl/c/syminver.pl
@@ -77,8 +77,134 @@ else {
     @sorted = sort @syms;
 }
 
+sub nameref {
+    my ($n, $none)=@_;
+    if($none) {
+        return $n;
+    }
+    if($n =~ /^CURLOPT_/) {
+        return "<a href=\"/libcurl/c/$n.html\">$n</a>";
+    }
+    elsif($n =~ /^CURLMOPT_/) {
+        return "<a href=\"/libcurl/c/$n.html\">$n</a>";
+    }
+    elsif($n =~ /^CURLINFO_/) {
+        return "<a href=\"/libcurl/c/$n.html\">$n</a>";
+    }
+    elsif($n =~ /^CURLALTSVC_/) {
+        return "<a href=\"/libcurl/c/CURLOPT_ALTSVC_CTRL.html\">$n</a>";
+    }
+    elsif($n =~ /^CURLAUTH_/) {
+        return "<a href=\"/libcurl/c/CURLOPT_HTTPAUTH.html\">$n</a>";
+    }
+    elsif($n =~ /^CURLFORM_/) {
+        return "<a href=\"/libcurl/c/curl_formadd.html\">$n</a>";
+    }
+    elsif($n =~ /^CURLKH/) {
+        return "<a href=\"/libcurl/c/CURLOPT_SSH_KEYFUNCTION.html\">$n</a>";
+    }
+    elsif($n =~ /^CURLE_/) {
+        return "<a href=\"/libcurl/c/libcurl-errors.html\">$n</a>";
+    }
+    elsif($n =~ /^CURLM_/) {
+        return "<a href=\"/libcurl/c/libcurl-errors.html\">$n</a>";
+    }
+    elsif($n =~ /^CURLUE_/) {
+        return "<a href=\"/libcurl/c/libcurl-errors.html\">$n</a>";
+    }
+    elsif($n =~ /^CURLHE_/) {
+        return "<a href=\"/libcurl/c/libcurl-errors.html\">$n</a>";
+    }
+    elsif($n =~ /^CURLSHE_/) {
+        return "<a href=\"/libcurl/c/libcurl-errors.html\">$n</a>";
+    }
+    elsif($n =~ /^CURLPROTO_/) {
+        return "<a href=\"/libcurl/c/CURLINFO_PROTOCOL.html\">$n</a>";
+    }
+    elsif($n =~ /^CURLPX_/) {
+        return "<a href=\"/libcurl/c/CURLINFO_PROXY_ERROR.html\">$n</a>";
+    }
+    elsif($n =~ /^CURLPROXY_/) {
+        return "<a href=\"/libcurl/c/CURLOPT_PROXYTYPE.html\">$n</a>";
+    }
+    elsif($n =~ /^CURLSSLBACKEND_/) {
+        return "<a href=\"/libcurl/c/curl_global_sslset.html\">$n</a>";
+    }
+    elsif($n =~ /^CURLSSLOPT_/) {
+        return "<a href=\"/libcurl/c/CURLOPT_SSL_OPTIONS.html\">$n</a>";
+    }
+    elsif($n =~ /^CURLUPART_/) {
+        return "<a href=\"/libcurl/c/curl_url_get.html\">$n</a>";
+    }
+    elsif($n =~ /^CURLU_/) {
+        return "<a href=\"/libcurl/c/curl_url_get.html\">$n</a>";
+    }
+    elsif($n =~ /^CURLVERSION_/) {
+        return "<a href=\"/libcurl/c/curl_version_info.html\">$n</a>";
+    }
+    elsif($n =~ /^CURLSHOPT_/) {
+        return "<a href=\"/libcurl/c/$n.html\">$n</a>";
+    }
+    elsif($n =~ /^CURLWS_/) {
+        return "<a href=\"/libcurl/c/curl_ws_send.html\">$n</a>";
+    }
+    elsif($n =~ /^CURL_FORMADD_/) {
+        return "<a href=\"/libcurl/c/curl_formadd.html\">$n</a>";
+    }
+    elsif($n =~ /^CURL_HTTPPOST_/) {
+        return "<a href=\"/libcurl/c/curl_formadd.html\">$n</a>";
+    }
+    elsif($n =~ /^CURL_GLOBAL_/) {
+        return "<a href=\"/libcurl/c/curl_global_init.html\">$n</a>";
+    }
+    elsif($n =~ /^CURL_HTTP_VERSION_/) {
+        return "<a href=\"/libcurl/c/CURLOPT_HTTP_VERSION.html\">$n</a>";
+    }
+    elsif($n =~ /^CURL_LOCK_/) {
+        return "<a href=\"/libcurl/c/CURLSHOPT_SHARE.html\">$n</a>";
+    }
+    elsif($n =~ /^CURL_SSLVERSION_/) {
+        return "<a href=\"/libcurl/c/CURLOPT_SSLVERSION.html\">$n</a>";
+    }
+    elsif($n =~ /^CURL_VERSION_/) {
+        return "<a href=\"/libcurl/c/curl_version_info.html\">$n</a>";
+    }
+    elsif($n =~ /^CURL_RTSPREQ_/) {
+        return "<a href=\"/libcurl/c/CURLOPT_RTSP_REQUEST.html\">$n</a>";
+    }
+    elsif($n =~ /^CURLH_/) {
+        return "<a href=\"/libcurl/c/curl_easy_header.html\">$n</a>";
+    }
+    elsif($n =~ /^CURL_TRAILERFUNC_/) {
+        return "<a href=\"/libcurl/c/CURLOPT_TRAILERFUNCTION.html\">$n</a>";
+    }
+    elsif($n =~ /^CURLOT_/) {
+        return "<a href=\"/libcurl/c/curl_easy_option_next.html\">$n</a>";
+    }
+    elsif($n =~ /^CURLFINFOFLAG_/) {
+        return "<a href=\"/libcurl/c/CURLOPT_CHUNK_BGN_FUNCTION.html\">$n</a>";
+    }
+    elsif($n =~ /^CURLFILETYPE_/) {
+        return "<a href=\"/libcurl/c/CURLOPT_CHUNK_BGN_FUNCTION.html\">$n</a>";
+    }
+    elsif($n =~ /^CURL_CHUNK_BGN_FUNC_/) {
+        return "<a href=\"/libcurl/c/CURLOPT_CHUNK_BGN_FUNCTION.html\">$n</a>";
+    }
+    elsif($n =~ /^CURL_CHUNK_END_FUNC_/) {
+        return "<a href=\"/libcurl/c/CURLOPT_CHUNK_END_FUNCTION.html\">$n</a>";
+    }
+    elsif($n =~ /^CURLSSH_AUTH_/) {
+        return "<a href=\"/libcurl/c/CURLOPT_SSH_AUTH_TYPES.html\">$n</a>";
+    }
+    elsif($n =~ /^CURL_POLL_/) {
+        return "<a href=\"/libcurl/c/CURLMOPT_SOCKETFUNCTION.html\">$n</a>";
+    }
+    return $n;
+}
+
 for my $s (@sorted) {
-    printf "<tr><td>$s</td><td>%s</td><td>%s</td><td>%s</td></tr>\n",
+    printf "<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>\n",
+        nameref($s, $slast{$s}),
         verlink($sintro{$s}),
         verlink($sdepr{$s}),
         verlink($slast{$s});
