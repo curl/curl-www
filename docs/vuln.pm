24ba9eb78991552287561a3a65c35755e1acf25f
Daniel Stenberg <daniel@haxx.se>
2024-07-23 23:42:05 +0200

    CVE-2024-6874.md: macidn punycode buffer overread

--- docs/vuln.pm
+++ docs/vuln.pm
@@ -16,6 +16,7 @@
 #
 # List of CWEs => https://cwe.mitre.org/data/definitions/658.html
 @vuln = (
+    "CVE-2024-6874.html|8.8.0|8.8.0|macidn punycode buffer overread|CVE-2024-6874|20240724|20240716|CWE-126: Buffer Over-read|540|IDN|OVERREAD|lib|low|https://hackerone.com/reports/2604391",
     "CVE-2024-6197.html|8.6.0|8.8.0|freeing stack buffer in utf8asn1str|CVE-2024-6197|20240724|20240619|CWE-590: Free of Memory not on the Heap|2540|ASN1|BAD_FREE|both|medium|https://hackerone.com/reports/2559516",
     "CVE-2024-2466.html|8.5.0|8.6.0|TLS certificate check bypass with mbedTLS|CVE-2024-2466|20240327|20240314|CWE-297: Improper Validation of Certificate with Host Mismatch|2540|TLS|-|both|medium|https://hackerone.com/reports/2416725",
     "CVE-2024-2398.html|7.44.0|8.6.0|HTTP/2 push headers memory-leak|CVE-2024-2398|20240327|20240305|CWE-772: Missing Release of Resource after Effective Lifetime|2540|HTTP/2|-|lib|medium|https://hackerone.com/reports/2402845",
