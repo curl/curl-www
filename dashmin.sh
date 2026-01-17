83430c96dd5d74a4efc2a184251a705d61121d4a
Viktor Szakats <commit@vsz.me>
2026-01-17 11:12:30 +0100

    typo

--- dashmin.sh
+++ dashmin.sh
@@ -26,7 +26,7 @@ if command -v scour >/dev/null 2>&1; then
   cat stats.list | while read -r f; do
     for pass in 1 2 3; do
       # `--set-precision 3` can make results smaller with the cost
-      # of a bit less precise graphs. Only noticable side-by-side.
+      # of a bit less precise graphs. Only noticeable side-by-side.
       scour \
         --set-precision 4 \
         --strip-xml-prolog \
