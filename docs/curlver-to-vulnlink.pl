b6b0bb04010d2e5b3f1568c0c8ac59f42b0b398d
Daniel Stenberg <daniel@haxx.se>
2026-08-21 23:23:50 +0200

    curlver-to-vulnlink.pl: use VERSIONS.md to match versions better
    By using the list of all known and existing version numbers, this script
    now only creates links to actual curl releases.

--- docs/curlver-to-vulnlink.pl
+++ docs/curlver-to-vulnlink.pl
@@ -1,12 +1,34 @@
 #!/usr/bin/env perl
 
+my $versions = shift @ARGV;
+
+if(!$versions) {
+    die "no VERSIONS.md path provided";
+}
+
+my %exists;
+open(V, "<$versions");
+while(<V>) {
+    if(/^- ([45678][0-9.]*):/) {
+        $exists{$1} = 1;
+    }
+}
+close(V);
+
+sub inject {
+    my ($version) = @_;
+    if($exists{$version}) {
+        return "<a href=\"vuln-$version.html\">$1<\/a>";
+    }
+    return $version;
+}
+
 while(<STDIN>) {
-    if($_ =~ s/([78]\.(\d+)\.(\d+))/<a href="vuln-$1.html">$1<\/a>/g) {
+    if($_ =~ s/([45678][0-9.]*)/inject($1)/eg) {
         # to avoid that 7.X matches a substring of a longer version like
         # 7.1 in 7.17.1
         ;
     }
-    $_ =~ s/(7\.[1-9])([^0-9\.])/<a href="vuln-$1.html">$1<\/a>$2/g;
     $_ =~ s/(CURLOPT_([A-Z0-9_]+))/<a href="https:\/\/curl.se\/libcurl\/c\/$1.html">$1<\/a>/g;
     print $_;
 }
